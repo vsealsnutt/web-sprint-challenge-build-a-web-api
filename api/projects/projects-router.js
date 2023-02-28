74eb6340b504248124126e463c4365a3bfdd7441
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-28 16:28:48 -0500

    wrote a get request for the projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,27 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Projects = require('./projects-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    const projects = await Projects.get()
+    try {
+        res.status(200).json(projects);
+    } catch(err) {
+        next(err);
+    }
+});
+
+// router.get();
+
+// router.post();
+
+// router.put();
+
+// router.delete();
+
+// router.get();
+
+module.exports = router;
